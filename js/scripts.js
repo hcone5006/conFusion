5d98d61cb5b829cf8eac6b46d18e31f3890247bc
HeidiC <heidic@traffic.net.nz>
2018-05-14 13:25:24 +1200

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    // Carousel functions
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+
+    // Modal functions 
+    $("#loginButton").click(function(){
+        $("#loginModal").modal();
+    });
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal();
+    });
+    
+});
